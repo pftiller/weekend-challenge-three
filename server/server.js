b04b379a6195a9302faaaa157dbd4171c918203b
pftiller <pftiller@gmail.com>
2018-01-15 19:01:17 -0600

    Heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+var port = process.env.PORT || 5000;
+app.listen(port, () => {
+console.log('server is up on: ', port);
+
+});
+
 const path = require( 'path' );
 
 app.use(express.static('server/public'));
@@ -10,8 +16,3 @@ const router = require('./routes/tasklist.router.js');
 app.use('/mytasklist', router);
 
 
-const port = 5000;
-app.listen(port, () => {
-console.log('server is up on: ', port);
-
-});
