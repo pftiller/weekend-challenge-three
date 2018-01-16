eeb0c6f0a10bf05b9ab594f060c1c5b00075c45d
pftiller <pftiller@gmail.com>
2018-01-15 19:17:47 -0600

    PS on Heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const Pool = pg.Pool;
 const config = {
-    database: 'mytasklist',
+    database: process.env.DATABASE_NAME || 'mytasklist',
     host: 'localhost',
     port: 5432,
     max: 10
